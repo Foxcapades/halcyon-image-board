61b8f1e9a68ec587d3420f38b1a6a073eeb676c8
Elizabeth Harper <foxcapade@gmail.com>
2010-03-02 17:06:41 +0000

    git-svn-id: svn://svn.code.sf.net/p/halcyoniboard/code/trunk@2 f401f003-0d26-452f-ac42-f9d2f38eefe1

---
+++ c/frm.php
@@ -0,0 +1,216 @@
+<?php
+/**
+ *
+ *	Taco Image Board
+ *  Copyright (C) 2009-2010  Steven K. Utiger
+ *
+ *    This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ *    This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ *    You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Form Validation Class
+ *
+ * 	This class will be used to validate user input throughout the site.  It
+ * 'should' simplify things later on.
+ *
+ * 	It should be noted that this validator is tailored specifically for this
+ * project, and is not likely to be usefull anywhere else.
+ *
+ */
+class FORM {
+
+	private $string;
+	private $altered;
+	public $error = FALSE;
+	public $invalid = FALSE;
+	public $reasons = array();
+	public $legal_chars = '/[^a-z0-9\v\-?`:;&~_!\-=+\\@#$%\^ *.	,<>\/()[\]]/i';
+
+	function __construct() {}
+
+	/**
+	 * Text Entry Validation
+	 *
+	 * 	Takes the given string, and verifies there are no 'Illegal' or problem
+	 * causing characters.
+	 *
+	 * @param string $input
+	 * @param int $minln
+	 * @param int $maxln
+	 * @return bool
+	 */
+
+	public function length($string,$min=4,$max=32) {
+		$string = strlen($string);
+		if($string > $max || $string < $min) {
+
+			$this->reasons[] = 'Invalid length, must be between '
+								.$min.' and '.$max.' characters.';
+			return FALSE;
+
+		} else {
+			return TRUE;
+		}
+
+	}
+	public function validate_text($input,$minln=4,$maxln=32) {
+
+		$res = preg_match_all($this->legal_chars,$input,$matches);
+
+		if($res == FALSE) {
+
+			$this->error = preg_last_error().' Preg_Match error.';
+
+		} elseif($res != 0) {
+
+			$invalid = TRUE;
+			$this->reasons[] = 'Illegal character.';
+
+		}
+
+		$invalid = (!$this->length($input,$minln,$maxln)) ? TRUE : FALSE;
+
+
+		if($invalid === TRUE) {
+
+			$this->invalid = TRUE;
+			return FALSE;
+
+		} else {
+
+			return TRUE;
+
+		}
+
+	}
+
+	/**
+	 * Input Scrubber
+	 *
+	 * 	'Cleans' the input to make it safe for storing and displaying.
+	 *
+	 * @param string $input
+	 * @return string
+	 */
+	public function scrub_text($input) {
+
+		$input = htmlentities($input, ENT_QUOTES);
+
+		$input = preg_replace($this->legal_chars,'',$input);
+
+		return $input;
+	}
+	public function validate_name(){}
+	public function validate_email($email){
+
+		if(!ereg("^[^@]{1,64}@[^@]{1,255}$", $email)) {
+
+			return false;
+
+		}
+
+		$email_array = explode("@", $email);
+		$local_array = explode(".", $email_array[0]);
+
+		for($i = 0; $i < sizeof($local_array); $i++) {
+
+			if(!ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$",$local_array[$i])) {
+
+				return false;
+
+			}
+		}
+
+		if(!ereg("^\[?[0-9\.]+\]?$", $email_array[1])) {
+
+			$domain_array = explode(".", $email_array[1]);
+
+			if(sizeof($domain_array) < 2) {
+
+				return false;
+
+			}
+
+			for($i = 0; $i < sizeof($domain_array); $i++) {
+
+				if(!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])| ([A-Za-z0-9]+))$",$domain_array[$i])) {
+
+					return false;
+
+				}
+			}
+		}
+
+		return true;
+	}
+	// TODO: Make this more 'all purpose'
+	public function file_check(&$upload,$allowedTypes = FALSE,&$errmes = ''){
+		
+		$output = TRUE;
+		
+		if($upload['error'] == UPLOAD_ERR_OK) {
+			if($allowedTypes == TRUE && !in_array($upload['type'],$allowedTypes)) {
+
+				$errmes = 'Invalid File Type, please choose another file and try again.';
+				return FALSE;
+
+			} else {
+				
+				return TRUE;
+				
+			}
+			
+		} elseif($upload['error'] == UPLOAD_ERR_NO_FILE){
+			return TRUE;
+		}
+		
+		switch($upload['error']) {
+
+			// File is too large
+			case UPLOAD_ERR_INI_SIZE:
+			case UPLOAD_ERR_FORM_SIZE:
+				$errmes = 'File too big, try resizing it or uploading something else.';
+				break;
+
+			// Partial file upload
+			case UPLOAD_ERR_PARTIAL:
+				$errmes = 'File errored while uploading, please try again.';
+				break;
+
+			// Missing a temporary folder
+			case UPLOAD_ERR_NO_TMP_DIR:
+				$errmes = 'Server side upload error. Error reported to admins, please check back later.';
+				ERROR::report('User upload failed, server said NO TEMP DIR.');
+				break;
+
+			// Failed to write file to disk
+			case UPLOAD_ERR_CANT_WRITE:
+				$errmes = 'Server side upload error. Error reported to admins, please check back later.';
+				ERROR::report('User upload failed, server said Failed to write to disk.');
+				break;
+
+			// File upload stopped by extension
+			case UPLOAD_ERR_EXTENSION:
+				$errmes = 'Server does not permit uploading files of the specified type. Error reported to admins, please choose another file or check back later.';
+				ERROR::report('User:'.$_SERVER['REMOTE_ADDR'].' tried to upload a restricted file type ('.$upload['type'].').');
+				break;
+
+			default:
+				break;
+		}
+		
+		return FALSE;
+	}
+
+}
+?>
---
+++ usr.php
@@ -0,0 +1,232 @@
+<?php
+/**
+ *
+ *	Taco Image Board
+ *  Copyright (C) 2009-2010  Steven K. Utiger
+ *
+ *    This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ *    This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ *    You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * User Management
+ *
+ * 	This page handles most of the user handling functions...
+ */
+
+session_start();
+
+/**
+ * Attempt to import the configuration file.
+ */
+if(file_exists('n/cnf.php')){require_once 'n/cnf.php';} else {die('dude, wtf');}
+// Set some base page variables
+$P->set('title',$VAR['site_title'].' / User Management');
+$P->set('h1','User Management');
+
+// Start the body
+$body = '<div class="body">'."\n";
+
+switch($_GET['mode']) {
+
+
+	// TODO: PHP, USER: Fix login script error handler
+	// Login
+	case 'login':
+
+		// Start the form variable array for the login form.
+		$formVars = array('action'=>'usr.php?mode=login');
+
+		$P->set('mes','User Login');
+
+		// Check to see if were trying to log the user in or not...
+		if(count($_POST)) {
+
+			$P->set('mes','Attempting Login...');
+
+			$e = 0;
+
+			// Clean these to make sure no one does anything funny...
+			$cleanUserName = $SQL->real_escape_string($_POST['unme']);
+			$cleanPassword = md5($_POST['upss']);
+
+			// More security stuff
+			$e += (!$FORM->length($cleanUserName)) ? 1 : 0;
+
+			if($e == 0) {
+
+				// Try and pull the user info from the database
+				$selUser = $SQL->query('SELECT * FROM `usr_accounts` WHERE `name` = \''.$cleanUserName.'\' AND `password` = \''.$cleanPassword.'\'');
+
+				// If the database returns a number other than 1 then something is wrong
+				if($selUser->num_rows != 1) {
+
+					// If the number is greater than 1, you have duplicate entries in your database.
+					if($selUser->num_rows > 1) {
+						ERROR::report('Duplicate entries found in database for User: '.$cleanUserName);
+					}
+
+					$e += 2;
+
+				} else {
+
+					$userRow = $selUser->fetch_row();
+					$_SESSION['uid'] = $userRow[0];
+					$_SESSION['password'] = $userRow[3];
+					$HERE = (strpos($_SESSION['return'],$VAR['base_url']) === FALSE) ? $VAR['base_url'] : $_SESSION['return'];
+					$P->set('headstuff','<meta http-equiv="refresh" content="3;url='.$HERE.'" />');
+					unset($_SESSION['return']);
+					$body .= '<p>Login successful.<br /><br />Redirecting to <a href="'.$HERE.'" class="cd">'.$HERE.'</a> after 3 seconds.</p>'."\n";
+					break;
+
+				}
+
+			}
+
+			$errors = array();
+			$c = array(
+				4 => 'SQL ERROR',
+				2 => 'Username & Password do not match.',
+				1 => 'Username Must be 4 - 32 characters.');
+
+			$formVars['unme'] = $cleanUserName;
+
+			krsort($c,SORT_NUMERIC);
+
+			foreach($c as $k => $v) {
+				if($e >= $k) {
+					$e -= $k;
+					$errors[] = $v;
+				}
+			}
+
+			$body .= '<p class="error">';
+			$body .= implode("<br />\n",$errors);
+			$body .= '</p>';
+
+		} else {
+				$_SESSION['return'] = $_SERVER['HTTP_REFERER'];
+			}
+
+
+
+		$P->formtovar('page_content','forms.php','login',$formVars);
+
+		unset($cleanPassword,$cleanUserName,$selUser,$userRow,$e,$c,$errors,$formVars);
+
+		$body .= $P->vars['page_content'];
+		break;
+
+	// Logout
+	case 'logout':
+		session_destroy();
+		$HERE = (strpos($_SERVER['HTTP_REFERER'],$VAR['base_url']) === FALSE) ? $VAR['base_url'] : $_SERVER['HTTP_REFERER'];
+		$P->set('headstuff','<meta http-equiv="refresh" content="3;url='.$HERE.'" />');
+		$body .= '<p>Session successfully terminated.<br /><br />Redirecting to <a href="'.$HERE.'" class="cd">'.$HERE.'</a> after 3 seconds.</p>';
+		break;
+
+	// New User Account
+	case 'nac':
+
+		$e = 0;
+		// Set some page variables
+		$P->set('mes','Register New Account');
+		$formVars = array('action'=>'usr.php?mode=nac');
+
+		// Check to see if the user has submited the form
+		if(count($_POST)) {
+
+			// Validate username
+			$e += (!$FORM->validate_text($_POST['unme'])) ? 1 : 0;
+
+			// Validate Email
+			$e += (!$FORM->validate_email($_POST['mail'])) ? 2 : 0;
+
+			// Verify passwords match
+			$e += ($_POST['upss'] != $_POST['vpss']) ? 4 : 0;
+
+			// If no errors have occured, then continue
+			if($e === 0) {
+
+				// TODO: HTML, FORMS: make a note for the user that mentions the passwords not being recoverable
+				// TODO: PHP, USER: make a password reset page
+				// TODO: PHP, USER: don't show the register page for logged in users to prevent problems
+				// TODO: PHP, USER: create user page
+				// TODO: PHP, ADMIN: set up email verification as an admin option
+				// TODO: PHP, MISC: Auto login on registration
+
+				$pass = md5($_POST['upss']);
+
+				// Try and insert the new user
+				if($SQL->query('INSERT INTO `usr_accounts` VALUES (NULL,\''.$_POST['unme'].'\',2,\''.$_POST['mail'].'\',\''.$pass.'\',0,\'anon.png\')')) {
+
+					// If it went without any problems, say so and end.
+					$body .= '<p class="green">Registration Complete, you may now log in.</p>';
+					break;
+
+				}
+				// If we hit this, there was an sql problem
+				ERROR::report('SQL Error on user registration, database said: '.$SQL->error);
+				$e += 8;
+			}
+			$errors = array();
+
+			// The error code array
+			$ErrorCodes = array(
+				1 => 'Invalid Username',
+				2 => 'Invalid Email Address',
+				4 => 'Passwords do not match',
+				8 => 'SQL Error, reported to admins'
+			);
+
+			krsort($ErrorCodes,SORT_NUMERIC);
+
+			foreach($ErrorCodes as $k => $v) {
+				if($e >= $k) {
+					$e -= $k;
+					$errors[] = $v;
+				}
+			}
+
+			// Let the user know what went wrong
+			$body .= '<p class="error">';
+			$body .= implode("<br />\n",$errors);
+			$body .= '</p>';
+
+
+			// Set some vars before returning the form
+			$formVars['unme'] = $FORM->scrub_text($_POST['unme']);
+			$formVars['mail'] = $FORM->scrub_text($_POST['mail']);
+
+		}
+
+		// If we have reached this then just show the form
+		$P->formtovar('page_content','forms.php','newuser');
+		$body .= $P->vars['page_content'];
+		break;
+
+	case 'uac':
+		$P->formtovar('page_content','forms.php','useracc');
+		$body .= $P->vars['page_content'];
+		break;
+
+	default:
+		$body .= 'Why are you here again?';
+		break;
+}
+$body .= "\n".'</div>';
+$navbar = navbuild($SQL);
+$P->set('navbar',$navbar);
+$P->set('body',$body);
+$P->load('base.php');
+$P->render();
+?>
