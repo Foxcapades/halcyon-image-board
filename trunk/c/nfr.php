cf35a0e1415ad4c41f0ec30b26bdb4ad4f8c698c
ryokotsusai <ryokotsusai@f401f003-0d26-452f-ac42-f9d2f38eefe1>
2010-03-09 21:57:34 +0000

    form class!

--- trunk/c/nfr.php
+++ trunk/c/nfr.php
@@ -18,8 +18,10 @@
  */
 class newForm {
 
-	public	$parts=array();
-	private $html,$fieldOpen = FALSE;
+public	$parts=array();
+private
+	$html,
+	$fieldOpen = FALSE;
 
 public function __construct
 (
@@ -34,162 +36,16 @@ public function __construct
 )
 {
 	$html  = '<form action="'.$action.'" method="'.$method.'"';
-	$html .= ($name != FALSE) ? ' name="'.$name.'"' : '';
-	$html .= ($id != FALSE) ? ' id="'.$id.'"' : '';
-	$html .= ($class != FALSE) ? ' class="'.$class.'"' : '';
-	$html .= ($accept != FALSE) ? ' accept="'.$accept.'"' : '';
-	$html .= ($charset != FALSE) ? ' accept-charset="'.$charset.'"' : '';
+	$html .= ($enctype	!== FALSE) ? ' enctype="'.$enctype.'"' : '';
+	$html .= ($name		!== FALSE) ? ' name="'.$name.'"' : '';
+	$html .= ($id		!== FALSE) ? ' id="'.$id.'"' : '';
+	$html .= ($class	!== FALSE) ? ' class="'.$class.'"' : '';
+	$html .= ($accept	!== FALSE) ? ' accept="'.$accept.'"' : '';
+	$html .= ($charset	!== FALSE) ? ' accept-charset="'.$charset.'"' : '';
 	$html .= ">\n\n";
 
 	$this->html = $html;
 }
-
-public function inputText
-(
-	$name,
-	$label		= FALSE,
-	$value		= FALSE,
-	$id			= FALSE,
-	$class		= FALSE,
-	$maxlength	= FALSE,
-	$disabled	= FALSE,
-	$readonly	= FALSE,
-	$size		= FALSE,
-	$return		= FALSE
-)
-{
-	$html  = '<div>';
-
-	$id = ($id === FALSE) ? $name : $id;
-
-	$html .= ($label !== FALSE) ? '<label for='.$id.'">'.$label.'</label>' : '';
-
-	$html .= '<input type="text" name="'.$name.'"';
-
-	$html .= ($value		!== FALSE)	? ' value="'.htmlentities($value).'"' : '';
-	$html .= ($id			!== FALSE)	? ' id="'.$id.'"' : '';
-	$html .= ($class		!== FALSE)	? ' class="'.$class.'"' : '';
-	$html .= ($maxlength	!== FALSE)	? ' maxlength="'.$maxlength.'"' : '';
-	$html .= ($disabled		=== TRUE)	? ' disabled="disabled"' : '';
-	$html .= ($readonly 	=== TRUE)	? ' readonly="readonly"' : '';
-	$html .= ($size 		!== FALSE)	? ' size="'.$size.'"' : '';
-	$html .= ' /></div>';
-
-	$this->html .= $html;
-	$parts[] = $html;
-	if($return) {return $html;}
-}
-
-public function inputPassword
-(
-	$name,
-	$label		= FALSE,
-	$value		= FALSE,
-	$id			= FALSE,
-	$class		= FALSE,
-	$maxlength	= FALSE,
-	$disabled	= FALSE,
-	$readonly	= FALSE,
-	$size		= FALSE,
-	$return		= FALSE)
-{
-	$html  = '<div>';
-	$id = ($id === FALSE) ? $name : $id;
-	$html .= ($label != FALSE) ? '<label for='.$id.'">'.$label.'</label>' : '';
-	$html .= '<input type="password" name="'.$name.'"';
-	$html .= ($value		!== FALSE)	? ' value="'.htmlentities($value).'"' : '';
-	$html .= ($id			!== FALSE)	? ' id="'.$id.'"' : '';
-	$html .= ($class		!== FALSE)	? ' class="'.$class.'"' : '';
-	$html .= ($maxlength	!== FALSE)	? ' maxlength="'.$maxlength.'"' : '';
-	$html .= ($disabled		=== TRUE)	? ' disabled="disabled"' : '';
-	$html .= ($readonly 	=== TRUE)	? ' readonly="readonly"' : '';
-	$html .= ($size 		!== FALSE)	? ' size="'.$size.'"' : '';
-	$html .= ' /></div>';
-		$this->html .= $html;
-	$parts[] = $html;
-	if($return) {return $html;}
-}
-
-public function inputCheckbox
-(
-	$name,
-	$value,
-	$label		=FALSE,
-	$id			=FALSE,
-	$class		=FALSE,
-	$checked	=FALSE,
-	$disabled	=FALSE,
-	$return		=FALSE
-)
-{
-	$html  = '<div>';
-	$id = ($id === FALSE) ? $name : $id;
-	$html .= ($label	!== FALSE)	? '<label for='.$id.'">'.$label.'</label>' : '';
-	$html .= '<input type="checkbox" name="'.$name.'"';
-	$html .= ' value="'.htmlentities($value).'"';
-	$html .= ($id		!== FALSE)	? ' id="'.$id.'"' : '';
-	$html .= ($class 	!== FALSE)	? ' class="'.$class.'"' : '';
-	$html .= ($checked	=== TRUE)	? ' checked="checked"' : '';
-	$html .= ($disabled === TRUE)	? ' disabled="disabled"' : '';
-	$html .= ' /></div>';
-	$this->html .= $html;
-	$parts[] = $html;
-	if($return) {return $html;}
-}
-
-public function inputRadio
-(
-	$name,
-	$value,
-	$label		=FALSE,
-	$id			=FALSE,
-	$class		=FALSE,
-	$checked	=FALSE,
-	$disabled	=FALSE,
-	$return		=FALSE
-)
-{
-	$html  = '<div>';
-	$id = ($id === FALSE) ? $name : $id;
-	$html .= ($label 	!= FALSE)	? '<label for='.$id.'">'.$label.'</label>' : '';
-	$html .= '<input type="radio" name="'.$name.'"';
-	$html .= ' value="'.htmlentities($value).'"';
-	$html .= ($id		!== FALSE)	? ' id="'.$id.'"' : '';
-	$html .= ($class	!== FALSE)	? ' class="'.$class.'"' : '';
-	$html .= ($checked	=== TRUE)	? ' checked="checked"' : '';
-	$html .= ($disabled === TRUE)	? ' disabled="disabled"' : '';
-	$html .= ' /></div>';
-	$this->html .= $html;
-	$parts[] = $html;
-	if($return) {return $html;}
-}
-
-public function inputSubmit
-(
-	$value		='Submit',
-	$name		=FALSE,
-	$label		=FALSE,
-	$id			=FALSE,
-	$class		=FALSE,
-	$disabled	=FALSE,
-	$return		=FALSE
-)
-{
-	$html  = '<div>';
-	$id = ($id === FALSE) ? $name : $id;
-	$html .= ($label	!== FALSE) ? '<label for='.$id.'">'.$label.'</label>' : '';
-	$html .= '<input type="submit" value="'.$value.'"';
-	$html .= ($name		!== FALSE) ? ' name="'.$name.'"' : '';
-	$html .= ($id		!== FALSE) ? ' id="'.$id.'"' : '';
-	$html .= ($class	!== FALSE) ? ' class="'.$class.'"' : '';
-	$html .= ($disabled === TRUE) ? ' disabled="disabled"' : '';
-	$html .= ' /></div>';
-	$this->html .= $html;
-	$parts[] = $html;
-	if($return) {return $html;}
-}
-
-public function inputHTML($html) {$this->html .= $html; $parts[] = $html;}
 
 public function divLast
 (
@@ -207,6 +63,327 @@ public function divLast
 	$parts[] = $html.end($parts[]).'</div>';
 	if($return) {return $html.end($parts[]).'</div>';}
 }
+
+public function inputButton
+(
+	$name,
+	$value,
+	$label		=FALSE,
+	$id			=FALSE,
+	$class		=FALSE,
+	$checked	=FALSE,
+	$disabled	=FALSE,
+	$extra		=FALSE,
+	$return		=FALSE
+)
+{
+	$id = ($id === FALSE) ? $name : $id;
+
+	$html  = '<div';
+	$html .= ($class 	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($id		!== FALSE)	? ' id="div_'.$id.'"' : '';
+	$html  .= '>';
+
+	$html .= ($label	!== FALSE)	? '<label for='.$id.'">'.$label.'</label>' : '';
+
+	$html .= '<input type="checkbox" name="'.$name.'"';
+	$html .= ' value="'.htmlentities($value).'"';
+	$html .= ($id		!== FALSE)	? ' id="'.$id.'"' : '';
+	$html .= ($class 	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($checked	=== TRUE)	? ' checked="checked"' : '';
+	$html .= ($disabled	=== TRUE)	? ' disabled="disabled"' : '';
+	$html .= ($extra	!== FALSE)	? ' '.$extra : '';
+	$html .= " /></div>\n\n";
+	$this->html .= $html;
+	$parts[] = $html;
+	if($return) {return $html;}
+}
+
+public function inputCheckbox
+(
+	$name,
+	$value,
+	$label		= FALSE,
+	$id			= FALSE,
+	$class		= FALSE,
+	$checked	= FALSE,
+	$disabled	= FALSE,
+	$extra		= FALSE,
+	$return		= FALSE
+)
+{
+	$id = ($id === FALSE) ? $name : $id;
+
+	$html  = '<div';
+	$html .= ($class 	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($id		!== FALSE)	? ' id="div_'.$id.'"' : '';
+	$html  .= '>';
+
+	$html .= ($label	!== FALSE)	? '<label for='.$id.'">'.$label.'</label>' : '';
+
+	$html .= '<input type="checkbox" name="'.$name.'"';
+	$html .= ' value="'.htmlentities($value).'"';
+	$html .= ($id		!== FALSE)	? ' id="'.$id.'"' : '';
+	$html .= ($class 	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($checked	=== TRUE)	? ' checked="checked"' : '';
+	$html .= ($disabled === TRUE)	? ' disabled="disabled"' : '';
+	$html .= ($extra	!== FALSE)	? ' '.$extra : '';
+	$html .= " /></div>\n\n";
+	$this->html .= $html;
+	$parts[] = $html;
+	if($return) {return $html;}
+}
+
+public function inputFile
+(
+	$name,
+	$label		= FALSE,
+	$id			= FALSE,
+	$class		= FALSE,
+	$accept		= FALSE,
+	$disabled	= FALSE,
+	$size		= FALSE,
+	$extra		= FALSE,
+	$return		= FALSE
+)
+{
+	$id = ($id === FALSE) ? $name : $id;
+
+	$html  = '<div';
+	$html .= ($class 	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($id		!== FALSE)	? ' id="div_'.$id.'"' : '';
+	$html  .= '>';
+
+	$html .= ($label !== FALSE) ? '<label for='.$id.'">'.$label.'</label>' : '';
+
+	$html .= '<input type="file" name="'.$name.'"';
+
+	$html .= ($id		!== FALSE)	? ' id="'.$id.'"' : '';
+	$html .= ($class	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($accept	!== FALSE)	? ' accept="'.$accept.'"' : '';
+	$html .= ($disabled	=== TRUE)	? ' disabled="disabled"' : '';
+	$html .= ($size 	!== FALSE)	? ' size="'.$size.'"' : '';
+	$html .= ($extra	!== FALSE)	? ' '.$extra : '';
+	$html .= " /></div>\n\n";
+
+	$this->html .= $html;
+	$parts[] = $html;
+	if($return) {return $html;}
+}
+
+public function inputHidden
+(
+	$name,
+	$value,
+	$id			= FALSE,
+	$class		= FALSE,
+	$extra		= FALSE,
+	$return		= FALSE
+)
+{
+	$id = ($id === FALSE) ? $name : $id;
+
+	$html .= '<input type="hidden" name="'.$name.'" value="'.htmlentities($value).'"';
+	$html .= ($id			!== FALSE)	? ' id="'.$id.'"' : '';
+	$html .= ($class		!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($extra		!== FALSE)	? ' '.$extra : '';
+	$html .= " />\n\n";
+
+	$this->html .= $html;
+	$parts[] = $html;
+	if($return) {return $html;}
+}
+
+public function inputPassword
+(
+	$name,
+	$label		= FALSE,
+	$value		= FALSE,
+	$id			= FALSE,
+	$class		= FALSE,
+	$maxlength	= FALSE,
+	$disabled	= FALSE,
+	$readonly	= FALSE,
+	$size		= FALSE,
+	$extra		= FALSE,
+	$return		= FALSE)
+{
+	$id = ($id === FALSE) ? $name : $id;
+
+	$html  = '<div';
+	$html .= ($class 	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($id		!== FALSE)	? ' id="div_'.$id.'"' : '';
+	$html  .= '>';
+
+	$html .= ($label != FALSE) ? '<label for='.$id.'">'.$label.'</label>' : '';
+
+	$html .= '<input type="password" name="'.$name.'"';
+	$html .= ($value		!== FALSE)	? ' value="'.htmlentities($value).'"' : '';
+	$html .= ($id			!== FALSE)	? ' id="'.$id.'"' : '';
+	$html .= ($class		!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($maxlength	!== FALSE)	? ' maxlength="'.$maxlength.'"' : '';
+	$html .= ($disabled		=== TRUE)	? ' disabled="disabled"' : '';
+	$html .= ($readonly 	=== TRUE)	? ' readonly="readonly"' : '';
+	$html .= ($size 		!== FALSE)	? ' size="'.$size.'"' : '';
+	$html .= ($extra		!== FALSE)	? ' '.$extra : '';
+	$html .= " /></div>\n\n";
+	$this->html .= $html;
+	$parts[] = $html;
+	if($return) {return $html;}
+}
+
+public function inputRadio
+(
+	$name,
+	$value,
+	$label		= FALSE,
+	$id			= FALSE,
+	$class		= FALSE,
+	$checked	= FALSE,
+	$disabled	= FALSE,
+	$extra		= FALSE,
+	$return		= FALSE
+)
+{
+	$id = ($id === FALSE) ? $name : $id;
+
+	$html  = '<div';
+	$html .= ($class 	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($id		!== FALSE)	? ' id="div_'.$id.'"' : '';
+	$html  .= '>';
+
+	$html .= ($label 	!= FALSE)	? '<label for='.$id.'">'.$label.'</label>' : '';
+
+	$html .= '<input type="radio" name="'.$name.'"';
+	$html .= ' value="'.htmlentities($value).'"';
+	$html .= ($id		!== FALSE)	? ' id="'.$id.'"' : '';
+	$html .= ($class	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($checked	=== TRUE)	? ' checked="checked"' : '';
+	$html .= ($disabled === TRUE)	? ' disabled="disabled"' : '';
+	$html .= ($extra	!== FALSE)	? ' '.$extra : '';
+	$html .= " /></div>\n\n";
+	$this->html .= $html;
+	$parts[] = $html;
+	if($return) {return $html;}
+}
+
+public function inputSubmit
+(
+	$value		='Submit',
+	$name		= FALSE,
+	$label		= FALSE,
+	$id			= FALSE,
+	$class		= FALSE,
+	$disabled	= FALSE,
+	$extra		= FALSE,
+	$return		= FALSE
+)
+{
+	$id = ($id === FALSE) ? $name : $id;
+
+	$html  = '<div';
+	$html .= ($class 	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($id		!== FALSE)	? ' id="div_'.$id.'"' : '';
+	$html  .= '>';
+
+	$html .= ($label	!== FALSE) ? '<label for='.$id.'">'.$label.'</label>' : '';
+
+	$html .= '<input type="submit" value="'.$value.'"';
+	$html .= ($name		!== FALSE) ? ' name="'.$name.'"' : '';
+	$html .= ($id		!== FALSE) ? ' id="'.$id.'"' : '';
+	$html .= ($class	!== FALSE) ? ' class="'.$class.'"' : '';
+	$html .= ($disabled === TRUE) ? ' disabled="disabled"' : '';
+	$html .= ($extra	!== FALSE)	? ' '.$extra : '';
+	$html .= " /></div>\n\n";
+	$this->html .= $html;
+	$parts[] = $html;
+	if($return) {return $html;}
+}
+
+public function inputText
+(
+	$name,
+	$label		= FALSE,
+	$value		= FALSE,
+	$id			= FALSE,
+	$class		= FALSE,
+	$maxlength	= FALSE,
+	$disabled	= FALSE,
+	$readonly	= FALSE,
+	$size		= FALSE,
+	$extra		= FALSE,
+	$return		= FALSE
+)
+{
+	$id = ($id === FALSE) ? $name : $id;
+
+	$html  = '<div';
+	$html .= ($class 	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($id		!== FALSE)	? ' id="div_'.$id.'"' : '';
+	$html  .= '>';
+
+	$html .= ($label !== FALSE) ? '<label for='.$id.'">'.$label.'</label>' : '';
+
+	$html .= '<input type="text" name="'.$name.'"';
+
+	$html .= ($value		!== FALSE)	? ' value="'.htmlentities($value).'"' : '';
+	$html .= ($id			!== FALSE)	? ' id="'.$id.'"' : '';
+	$html .= ($class		!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($maxlength	!== FALSE)	? ' maxlength="'.$maxlength.'"' : '';
+	$html .= ($disabled		=== TRUE)	? ' disabled="disabled"' : '';
+	$html .= ($readonly 	=== TRUE)	? ' readonly="readonly"' : '';
+	$html .= ($size 		!== FALSE)	? ' size="'.$size.'"' : '';
+	$html .= ($extra		!== FALSE)	? ' '.$extra : '';
+	$html .= " /></div>\n\n";
+
+	$this->html .= $html;
+	$parts[] = $html;
+	if($return) {return $html;}
+}
+
+public function inputTextarea
+(
+	$name,
+	$label		= FALSE,
+	$value		= FALSE,
+	$cols		= 20,
+	$rows		= 4,
+	$id			= FALSE,
+	$class		= FALSE,
+	$disabled	= FALSE,
+	$readonly	= FALSE,
+	$extra		= FALSE,
+	$return		= FALSE
+)
+{
+	$id = ($id === FALSE) ? $name : $id;
+
+	$html  = '<div';
+	$html .= ($class 	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($id		!== FALSE)	? ' id="div_'.$id.'"' : '';
+	$html  .= '>';
+
+	$html .= ($label !== FALSE) ? '<label for='.$id.'">'.$label.'</label>' : '';
+
+	$html .= '<textarea name="'.$name.'"';
+	$html .= ' cols="'.$cols.'"';
+	$html .= ' rows="'.$rows.'"';
+
+	$html .= ($id		!== FALSE)	? ' id="'.$id.'"' : '';
+	$html .= ($class	!== FALSE)	? ' class="'.$class.'"' : '';
+	$html .= ($disabled	=== TRUE)	? ' disabled="disabled"' : '';
+	$html .= ($readonly	=== TRUE)	? ' readonly="readonly"' : '';
+	$html .= ($extra	!== FALSE)	? ' '.$extra : '';
+	$html .= '>';
+	$html .= ($value	!== FALSE)	? htmlentities($value) : '';
+	$html .= "</textarea></div>\n\n";
+
+	$this->html .= $html;
+	$parts[] = $html;
+	if($return) {return $html;}
+}
+
+public function inputHTML($html) {$this->html .= $html; $parts[] = $html;}
 
 public function labelBegin
 (
@@ -247,9 +424,9 @@ public function fieldStart
 	$return	= FALSE
 )
 {
-	$html = ($this->fieldOpen) ? '</fieldset>' : '';
+	$html = ($this->fieldOpen) ? "</fieldset>\n\n" : '';
 	$html .= '<fieldset>';
-	$html .= ($legend != FALSE) ? '<legend>'.$legend.'</legend>' : '';
+	$html .= ($legend != FALSE) ? '<legend>'.$legend.'</legend>'."\n" : '';
 
 	$this->html .= $html;
 	$parts[] = $html;
@@ -259,7 +436,7 @@ public function fieldStart
 
 public function fieldEnd($return=FALSE)
 {
-	$html = '</fieldset>';
+	$html = "</fieldset>\n\n";
 	$this->fieldOpen = FALSE;
 }
 
@@ -267,7 +444,12 @@ public function formReturn()
 {
 	$html = ($this->fieldOpen) ? '</fieldset></form>' : '</form>';
 	return $this->html.$html;
+}public function returnForm()
+{
+	$html = ($this->fieldOpen) ? '</fieldset></form>' : '</form>';
+	return $this->html.$html;
 }
+
 //EOC
 }
 ?>
--- trunk/t.php
+++ trunk/t.php
@@ -16,168 +16,167 @@
  *    You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 session_start();
-
 /**
  * Attempt to import the configuration file.
- */
+**/
 if(file_exists('n/cnf.php')){require_once 'n/cnf.php';} else {die('dude, wtf');}
-
 /**
- * Attempt to find the post class
+ * Verify and import the post class
  */
-if(file_exists('c/pst.php')){require_once 'c/pst.php';} else {
-	ERROR::dead('Could not find post creation class.');}
+if(file_exists('c/pst.php')){require_once 'c/pst.php';} else
+{ERROR::dead('Could not find post creation class.');}
 
-	$tid = $SQL->real_escape_string($_GET['tid']);
+$threadID = $SQL->real_escape_string($_GET['tid']);
 
-	if(!is_numeric($tid)) {
-		ERROR::report($tid.' given for thread ID by '.$_SERVER['REMOTE_ADDR']);
-		index();
-	}
+if(!is_numeric($threadID)) {
+	ERROR::report($threadID.' given for thread ID by '.$_SERVER['REMOTE_ADDR']);
+	index();
+}
 
-	$r = $SQL->query('SELECT * FROM `pst_threads` `p` INNER JOIN `ste_boards` `s` ON `p`.`board` = `s`.`id` WHERE `tid` = \''.$tid.'\' LIMIT 0,1');
+$r = $SQL->query('SELECT * FROM `pst_threads` `p` INNER JOIN `ste_boards` `s` ON `p`.`board` = `s`.`id` WHERE `tid` = \''.$threadID.'\' LIMIT 0,1');
 
-	$BINFO = $r->fetch_assoc();
+$BINFO = $r->fetch_assoc();
 
-	$P->set('title',$VAR['site_title'].' / '.$BINFO['title']);
-	$P->set('h1',$BINFO['title']);
+$P->set('title',$VAR['site_title'].' / '.$BINFO['title']);
+$P->set('h1',$BINFO['title']);
 
-	$P->set('navbar',navbuild($SQL));
-
-	$formVars = array(
-	'action'	=> $_SERVER['REQUEST_URI'].'&amp;post=new',
-	'ttle'		=> $BINFO['title'],
-	'unme'		=> $USR['name']
-	);
-
-	/*
-	 *
-	 * NEW POST HANDLING
-	 *
-	 */
-	if(count($_POST) && $_GET['post'] == 'new' && $USR['level'] >= $BINFO['reply'] && (time() - $_SESSION['postcooldown']) > 1) {
-
-		$continue = TRUE;
-		$reasons = array();
-
-		// Make sure users are allowed to upload files at the size you set
-		ini_set('upload_max_filesize',$VAR['maxFileSize']);
-		$post	= &$_POST;
-		$file	= &$_FILES;
-
-		$imageTypes = array('image/gif','image/jpeg','image/png');
-
-		$text = $FORM->scrub_text($post['text']);
-		if(strlen($text) < 3) {$continue = FALSE; $reasons[] = "Posts must be at least 3 characters.";}
+$P->set('navbar',navbuild($SQL));
 
 
-		/**
-		 * 	This trys to figure out and report errors.  Important, errors that were not
-		 * caused by normal user error are reported to the error log.
-		 */
 
-		// Check For File Upload Errors:
-		$errmes='';
-		$FORM->file_check($file['img1'],$imageTypes,$errmes);
-		$reasons[] = $errmes;
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-		// Generate a 24 random digit file name
-		$fname = ($file['img1']['error'] != UPLOAD_ERR_NO_FILE) ? rand_str().'.'.array_pop(explode('.',$file['img1']['name'])) : NULL;
+* New Post Handler
 
-		// Target upload directory + Generated file name
-		$newfile = ($file['img1']['error'] != UPLOAD_ERR_NO_FILE) ? $VAR['updir'] .= $fname : NULL;
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+if(count($_POST) && $_GET['post'] == 'new' && $USR['level'] >= $BINFO['reply'] && (time() - $_SESSION['postcooldown']) > 1) {
 
-		// Stop if the file is too big
-		if($file['img1']['size'] > $VAR['maxFileSize']) {
+	$continue = TRUE;
+	$reasons = array();
 
-			$reasons[] = 'File too big, try resizing it or uploading something else.';
-			$continue = FALSE;
+	// Make sure users are allowed to upload files at the size you set
+	ini_set('upload_max_filesize',$VAR['maxFileSize']);
+	$post	= &$_POST;
+	$file	= &$_FILES;
 
-		}
+	$imageTypes = array('image/gif','image/jpeg','image/png');
 
-		// Check if we should continue or not
-		if($continue && $file['img1']['error'] != UPLOAD_ERR_NO_FILE) {
+	$text = $FORM->scrub_text($post['text']);
+	if(strlen($text) < 3) {$continue = FALSE; $reasons[] = "Posts must be at least 3 characters.";}
 
-			// Try and move the uploaded file
-			if(!move_uploaded_file($file['img1']['tmp_name'],$newfile)) {
-				$continue = FALSE;
-				$reasons[] = 'Could not relocate uploaded file. Error reported, please try again later.';
-				ERROR::report('Failed to move an uploaded file');
-
-			} else {
-
-				if(!makethumb($newfile,$VAR['thdir'].$fname)) {
-					echo '<div class="error">Thumbnail image failed. Generic image used.</div>';
-					ERROR::report('Failed to create thumb for image '.$newfile);
-				}
-			}
-		}
-
-		if($continue && $_GET['post'] == 'new') {
-
-			if(!$SQL->query('INSERT INTO `pst_posts` (`thread`,`poster`,`image`,`text`) VALUES (\''.$tid.'\',\''.$USR['id'].'\',\''.$fname.'\',\''.$text.'\')')) {
-				$continue = FALSE;
-				$reasons[] = 'Database error, could not create post. Please try again later.';
-				ERROR::report('Could not create post, attempting to remove traces. DB said: '.$SQL->error);
-
-				if(!unlink($newfile)) {
-					ERROR::report('Could not delete uploaded file after post failed. File: '.$newfile);
-				}
-			} else {
-				if(!$SQL->query('UPDATE `pst_threads` SET `posted` = DEFAULT WHERE `tid` = \''.$BINFO['tid'].'\'')) {
-					ERROR::report('Could not update thread posted time on '.$BINFO['tid']);
-				}
-				$refreshq = $SQL->query('SELECT `pid` FROM `pst_posts` WHERE `poster` = \''.$USR['id'].'\' AND `thread` = \''.$tid.'\' ORDER BY `post_time` DESC LIMIT 0,1');
-				$refresha = $refreshq->fetch_assoc();
-				$_SESSION['postcooldown'] = time();
-				$P->set('headstuff','<meta http-equiv="refresh" content="0;url='.$VAR['base_url'].'/t.php?tid='.$tid.'#i'.$refresha['pid'].'" />');
-			}
-		}
-
-		/**
-		 * 	If they made it this far with no errors, I guess the post was made
-		 * successfully :P
-		 */
-
-		// if it errored out, there are error messages to display
-		if(!$continue) {
-			$errorBoxHtml = '<div class="error"><ul>'."\n";
-			foreach($reasons as $reason) {
-				$errorBoxHtml .= '<li>'.$reason.'</li>'."\n";
-			}
-			$errorBoxHtml .= '<ul></div>'."\n";
-		}
-
-	}
-	ini_restore('upload_max_filesize');
 
 	/**
-	 * If the thread ID given is not a number, then somethings up, so instead we'll
-	 * just show the index, and report the issue.
+	 * 	This trys to figure out and report errors.  Important, errors that were not
+	 * caused by normal user error are reported to the error log.
 	 */
 
-	$q = $SQL->query('SELECT `p`.*, `u`.* FROM `pst_posts` as `p` INNER JOIN `usr_accounts` as `u` ON `p`.`poster` = `u`.`id` WHERE `p`.`thread` = \''.$tid.'\' ORDER BY `pid` ASC');
+	// Check For File Upload Errors:
+	$errmes='';
+	$FORM->file_check($file['img1'],$imageTypes,$errmes);
+	$reasons[] = $errmes;
 
-	$body = $errorBoxHtml;
+	// Generate a 24 random digit file name
+	$fname = ($file['img1']['error'] != UPLOAD_ERR_NO_FILE) ? rand_str().'.'.array_pop(explode('.',$file['img1']['name'])) : NULL;
 
+	// Target upload directory + Generated file name
+	$newfile = ($file['img1']['error'] != UPLOAD_ERR_NO_FILE) ? $VAR['updir'] .= $fname : NULL;
 
-	$body .= '<div id="thread">'."\n";
-	$now = 1;
-	while($ch = $q->fetch_assoc()) {
-		$ch['text'] = $BBC->parse($ch['text']);
-		$derp = new POST($ch['id'], $ch['name'], $ch['avatar'], $ch['level'], $ch['email'], $ch['pid'], $ch['post_time'], $ch['text'], $ch['image'], $ch['tid']);
-		$body .= $derp->postbox();
-		if($USR['level'] >= $BINFO['reply']&& $now == 1) {
-			$body .= $P->formtovar('nerds','forms.php','newpost',$formVars);
-			$now --;
+	// Stop if the file is too big
+	if($file['img1']['size'] > $VAR['maxFileSize']) {
+
+		$reasons[] = 'File too big, try resizing it or uploading something else.';
+		$continue = FALSE;
+
+	}
+
+	// Check if we should continue or not
+	if($continue && $file['img1']['error'] != UPLOAD_ERR_NO_FILE) {
+
+		// Try and move the uploaded file
+		if(!move_uploaded_file($file['img1']['tmp_name'],$newfile)) {
+			$continue = FALSE;
+			$reasons[] = 'Could not relocate uploaded file. Error reported, please try again later.';
+			ERROR::report('Failed to move an uploaded file');
+
+		} else {
+
+			if(!makethumb($newfile,$VAR['thdir'].$fname)) {
+				echo '<div class="error">Thumbnail image failed. Generic image used.</div>';
+				ERROR::report('Failed to create thumb for image '.$newfile);
+			}
 		}
 	}
 
-	$body .= '</div>'."\n";
+	if($continue && $_GET['post'] == 'new') {
 
-	$P->set('body',$body);
-	$P->load('base.php');
-	$P->render();
-	?>
+		if(!$SQL->query('INSERT INTO `pst_posts` (`thread`,`poster`,`image`,`text`) VALUES (\''.$threadID.'\',\''.$USR['id'].'\',\''.$fname.'\',\''.$text.'\')')) {
+			$continue = FALSE;
+			$reasons[] = 'Database error, could not create post. Please try again later.';
+			ERROR::report('Could not create post, attempting to remove traces. DB said: '.$SQL->error);
+
+			if(!unlink($newfile)) {
+				ERROR::report('Could not delete uploaded file after post failed. File: '.$newfile);
+			}
+		} else {
+			if(!$SQL->query('UPDATE `pst_threads` SET `posted` = DEFAULT WHERE `tid` = \''.$BINFO['tid'].'\'')) {
+				ERROR::report('Could not update thread posted time on '.$BINFO['tid']);
+			}
+			$refreshq = $SQL->query('SELECT `pid` FROM `pst_posts` WHERE `poster` = \''.$USR['id'].'\' AND `thread` = \''.$threadID.'\' ORDER BY `post_time` DESC LIMIT 0,1');
+			$refresha = $refreshq->fetch_assoc();
+			$_SESSION['postcooldown'] = time();
+			$P->set('headstuff','<meta http-equiv="refresh" content="0;url='.$VAR['base_url'].'/t.php?tid='.$threadID.'#i'.$refresha['pid'].'" />');
+		}
+	}
+
+	/**
+	 * 	If they made it this far with no errors, I guess the post was made
+	 * successfully :P
+	 */
+
+	// if it errored out, there are error messages to display
+	if(!$continue) {
+		$errorBoxHtml = '<div class="error"><ul>'."\n";
+		foreach($reasons as $reason) {
+			$errorBoxHtml .= '<li>'.$reason.'</li>'."\n";
+		}
+		$errorBoxHtml .= '<ul></div>'."\n";
+	}
+
+}
+ini_restore('upload_max_filesize');
+
+/**
+ * If the thread ID given is not a number, then somethings up, so instead we'll
+ * just show the index, and report the issue.
+ */
+
+$q = $SQL->query('SELECT `p`.*, `u`.* FROM `pst_posts` as `p` INNER JOIN `usr_accounts` as `u` ON `p`.`poster` = `u`.`id` WHERE `p`.`thread` = \''.$threadID.'\' ORDER BY `pid` ASC');
+
+$body = $errorBoxHtml;
+
+
+$body .= '<div id="thread">'."\n";
+$now = 1;
+while($ch = $q->fetch_assoc()) {
+	$ch['text'] = $BBC->parse($ch['text']);
+	$derp = new POST($ch['id'], $ch['name'], $ch['avatar'], $ch['level'], $ch['email'], $ch['pid'], $ch['post_time'], $ch['text'], $ch['image'], $ch['tid']);
+	$body .= $derp->postbox();
+	if($USR['level'] >= $BINFO['reply']&& $now == 1) {
+		$postForm = new newForm($_SERVER['REQUEST_URI'].'&amp;post=new','post','multipart/form-data');
+		$postForm->fieldStart('Reply');
+		$postForm->inputTextarea('text','Text',FALSE,30);
+		$postForm->inputHidden('MAX_FILE_SIZE','2621440');
+		$postForm->inputFile('img1','Image');
+		$postForm->inputSubmit();
+		$body .= $postForm->returnForm();
+		$now --;
+	}
+}
+
+$body .= '</div>'."\n";
+
+$P->set('body',$body);
+$P->load('base.php');
+$P->render();
+?>
