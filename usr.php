936af4c3073b3c7b873b451026227d78f3b0e607
Elizabeth Harper <foxcapade@gmail.com>
2010-03-09 21:57:34 +0000

    form class!
    git-svn-id: svn://svn.code.sf.net/p/halcyoniboard/code/trunk@15 f401f003-0d26-452f-ac42-f9d2f38eefe1

--- usr.php
+++ usr.php
@@ -39,198 +39,202 @@ $body = '<div class="body">'."\n";
 switch($_GET['mode']) {
 
 
-	// TODO: PHP, USER: Fix login script error handler
-	// Login
-	case 'login':
+// TODO: PHP, USER: Fix login script error handler
+// Login
+case 'login':
 
-		// Start the form variable array for the login form.
+	// Start the form variable array for the login form.
 
-		$P->set('mes','User Login');
+	$P->set('mes','User Login');
 
-		// Check to see if were trying to log the user in or not...
-		if(count($_POST)) {
+	// Check to see if were trying to log the user in or not...
+	if(count($_POST)) {
 
-			$P->set('mes','Attempting Login...');
-
-			$e = 0;
-
-			// Clean these to make sure no one does anything funny...
-			$cleanUserName = $SQL->real_escape_string($_POST['unme']);
-			$cleanPassword = md5($_POST['upss']);
-
-			// More security stuff
-			$e += (!$FORM->length($cleanUserName)) ? 1 : 0;
-
-			if($e == 0) {
-
-				// Try and pull the user info from the database
-				$selUser = $SQL->query('SELECT * FROM `usr_accounts` WHERE `name` = \''.$cleanUserName.'\' AND `password` = \''.$cleanPassword.'\'');
-
-				// If the database returns a number other than 1 then something is wrong
-				if($selUser->num_rows != 1) {
-
-					// If the number is greater than 1, you have duplicate entries in your database.
-					if($selUser->num_rows > 1) {
-						ERROR::report('Duplicate entries found in database for User: '.$cleanUserName);
-					}
-
-					$e += 2;
-
-				} else {
-
-					$userRow = $selUser->fetch_row();
-					$_SESSION['uid'] = $userRow[0];
-					$_SESSION['password'] = $userRow[3];
-					$HERE = (strpos($_SESSION['return'],$VAR['base_url']) === FALSE) ? $VAR['base_url'] : $_SESSION['return'];
-					$P->set('headstuff','<meta http-equiv="refresh" content="3;url='.$HERE.'" />');
-					unset($_SESSION['return']);
-					$body .= '<p>Login successful.<br /><br />Redirecting to <a href="'.$HERE.'" class="cd">'.$HERE.'</a> after 3 seconds.</p>'."\n";
-					break;
-
-				}
-
-			}
-
-			$errors = array();
-			$c = array(
-			4 => 'SQL ERROR',
-			2 => 'Username & Password do not match.',
-			1 => 'Username Must be 4 - 32 characters.');
-
-			$formVars['unme'] = $cleanUserName;
-
-			krsort($c,SORT_NUMERIC);
-
-			foreach($c as $k => $v) {
-				if($e >= $k) {
-					$e -= $k;
-					$errors[] = $v;
-				}
-			}
-
-			$body .= '<p class="error">';
-			$body .= implode("<br />\n",$errors);
-			$body .= '</p>';
-
-		} else {
-			$_SESSION['return'] = $_SERVER['HTTP_REFERER'];
-		}
-
-
-
-		$form = new newForm('usr.php?mode=login','post',FALSE,FALSE,FALSE,'miniform');
-		$form->fieldstart('Login');
-		$form->inputText('unme','Username');
-		$form->inputPassword('upss','Password');
-		$form->inputSubmit('Login');
-		$form->inputHTML('<a href="usr.php?mode=nac" title="Register a new account.">Register New Account</a>');
-		$Lform = $form->formReturn();
-		$P->set('page_content',$Lform);
-
-		unset($cleanPassword,$cleanUserName,$selUser,$userRow,$e,$c,$errors,$formVars);
-
-		$body .= $P->vars['page_content'];
-		break;
-
-		// Logout
-	case 'logout':
-		session_destroy();
-		$HERE = (strpos($_SERVER['HTTP_REFERER'],$VAR['base_url']) === FALSE) ? $VAR['base_url'] : $_SERVER['HTTP_REFERER'];
-		$P->set('headstuff','<meta http-equiv="refresh" content="3;url='.$HERE.'" />');
-		$body .= '<p>Session successfully terminated.<br /><br />Redirecting to <a href="'.$HERE.'" class="cd">'.$HERE.'</a> after 3 seconds.</p>';
-		break;
-
-		// New User Account
-	case 'nac':
+		$P->set('mes','Attempting Login...');
 
 		$e = 0;
-		// Set some page variables
-		$P->set('mes','Register New Account');
-		$formVars = array('action'=>'usr.php?mode=nac');
 
-		// Check to see if the user has submited the form
-		if(count($_POST)) {
+		// Clean these to make sure no one does anything funny...
+		$cleanUserName = $SQL->real_escape_string($_POST['unme']);
+		$cleanPassword = md5($_POST['upss']);
 
-			// Validate username
-			$e += (!$FORM->validate_text($_POST['unme'])) ? 1 : 0;
+		// More security stuff
+		$e += (!$FORM->length($cleanUserName)) ? 1 : 0;
 
-			// Validate Email
-			$e += (!$FORM->validate_email($_POST['mail'])) ? 2 : 0;
+		if($e == 0) {
 
-			// Verify passwords match
-			$e += ($_POST['upss'] != $_POST['vpss']) ? 4 : 0;
+			// Try and pull the user info from the database
+			$selUser = $SQL->query('SELECT * FROM `usr_accounts` WHERE `name` = \''.$cleanUserName.'\' AND `password` = \''.$cleanPassword.'\'');
 
-			// If no errors have occured, then continue
-			if($e === 0) {
+			// If the database returns a number other than 1 then something is wrong
+			if($selUser->num_rows != 1) {
 
-				// TODO: HTML, FORMS: make a note for the user that mentions the passwords not being recoverable
-				// TODO: PHP, USER: make a password reset page
-				// TODO: PHP, USER: don't show the register page for logged in users to prevent problems
-				// TODO: PHP, USER: create user page
-				// TODO: PHP, ADMIN: set up email verification as an admin option
-				// TODO: PHP, MISC: Auto login on registration
-
-				$pass = md5($_POST['upss']);
-
-				// Try and insert the new user
-				if($SQL->query('INSERT INTO `usr_accounts` VALUES (NULL,\''.$_POST['unme'].'\',2,\''.$_POST['mail'].'\',\''.$pass.'\',0,\'anon.png\')')) {
-
-					// If it went without any problems, say so and end.
-					$body .= '<p class="green">Registration Complete, you may now log in.</p>';
-					break;
-
+				// If the number is greater than 1, you have duplicate entries in your database.
+				if($selUser->num_rows > 1) {
+					ERROR::report('Duplicate entries found in database for User: '.$cleanUserName);
 				}
-				// If we hit this, there was an sql problem
-				ERROR::report('SQL Error on user registration, database said: '.$SQL->error);
-				$e += 8;
+
+				$e += 2;
+
+			} else {
+
+				$userRow = $selUser->fetch_row();
+				$_SESSION['uid'] = $userRow[0];
+				$_SESSION['password'] = $userRow[3];
+				$HERE = (strpos($_SESSION['return'],$VAR['base_url']) === FALSE) ? $VAR['base_url'] : $_SESSION['return'];
+				$P->set('headstuff','<meta http-equiv="refresh" content="3;url='.$HERE.'" />');
+				unset($_SESSION['return']);
+				$body .= '<p>Login successful.<br /><br />Redirecting to <a href="'.$HERE.'" class="cd">'.$HERE.'</a> after 3 seconds.</p>'."\n";
+				break;
+
 			}
-			$errors = array();
-
-			// The error code array
-			$ErrorCodes = array(
-			1 => 'Invalid Username',
-			2 => 'Invalid Email Address',
-			4 => 'Passwords do not match',
-			8 => 'SQL Error, reported to admins'
-			);
-
-			krsort($ErrorCodes,SORT_NUMERIC);
-
-			foreach($ErrorCodes as $k => $v) {
-				if($e >= $k) {
-					$e -= $k;
-					$errors[] = $v;
-				}
-			}
-
-			// Let the user know what went wrong
-			$body .= '<p class="error">';
-			$body .= implode("<br />\n",$errors);
-			$body .= '</p>';
-
-
-			// Set some vars before returning the form
-			$formVars['unme'] = $FORM->scrub_text($_POST['unme']);
-			$formVars['mail'] = $FORM->scrub_text($_POST['mail']);
 
 		}
 
-		// If we have reached this then just show the form
-		$P->formtovar('page_content','forms.php','newuser');
-		$body .= $P->vars['page_content'];
-		break;
+		$errors = array();
+		$c = array(
+		4 => 'SQL ERROR',
+		2 => 'Username & Password do not match.',
+		1 => 'Username Must be 4 - 32 characters.');
 
-	case 'uac':
-		$form = new newForm();
-		$form->fieldStart('Avatar Settings');
-		$form->inputCheckbox('grav',1,'Use Gravatar?',FALSE,FALSE,TRUE);
-		$form->inputSubmit();
-		$body .= $form->formReturn();
-		break;
+		$formVars['unme'] = $cleanUserName;
 
-	default:
-		$body .= 'Why are you here again?';
-		break;
+		krsort($c,SORT_NUMERIC);
+
+		foreach($c as $k => $v) {
+			if($e >= $k) {
+				$e -= $k;
+				$errors[] = $v;
+			}
+		}
+
+		$body .= '<p class="error">';
+		$body .= implode("<br />\n",$errors);
+		$body .= '</p>';
+
+	} else {
+		$_SESSION['return'] = $_SERVER['HTTP_REFERER'];
+	}
+
+
+
+	$form = new newForm('usr.php?mode=login','post',FALSE,FALSE,FALSE,'miniform');
+	$form->fieldstart('Login');
+	$form->inputText('unme','Username');
+	$form->inputPassword('upss','Password');
+	$form->inputSubmit('Login');
+	$form->inputHTML('<a href="usr.php?mode=nac" title="Register a new account.">Register New Account</a>');
+	$Lform = $form->formReturn();
+	$P->set('page_content',$Lform);
+
+	unset($cleanPassword,$cleanUserName,$selUser,$userRow,$e,$c,$errors,$formVars);
+
+	$body .= $P->vars['page_content'];
+	break;
+
+	// Logout
+case 'logout':
+	session_destroy();
+	$HERE = (strpos($_SERVER['HTTP_REFERER'],$VAR['base_url']) === FALSE) ? $VAR['base_url'] : $_SERVER['HTTP_REFERER'];
+	$P->set('headstuff','<meta http-equiv="refresh" content="3;url='.$HERE.'" />');
+	$body .= '<p>Session successfully terminated.<br /><br />Redirecting to <a href="'.$HERE.'" class="cd">'.$HERE.'</a> after 3 seconds.</p>';
+	break;
+
+	// New User Account
+case 'nac':
+
+	$e = 0;
+	// Set some page variables
+	$P->set('mes','Register New Account');
+	$regForm = new newForm('usr.php?mode=nac');
+	$regForm->fieldStart('New User Registration');
+	$regForm->inputText('unme','Username');
+	$regForm->inputText('mail','Email');
+	$regForm->inputPassword('upss','Password');
+	$regForm->inputPassword('vpss','Verify Password');
+	$regForm->inputSubmit('Register');
+		// Check to see if the user has submited the form
+	if(count($_POST)) {
+
+		// Validate username
+		$e += (!$FORM->validate_text($_POST['unme'])) ? 1 : 0;
+
+		// Validate Email
+		$e += (!$FORM->validate_email($_POST['mail'])) ? 2 : 0;
+
+		// Verify passwords match
+		$e += ($_POST['upss'] != $_POST['vpss']) ? 4 : 0;
+
+		// If no errors have occured, then continue
+		if($e === 0) {
+
+			// TODO: HTML, FORMS: make a note for the user that mentions the passwords not being recoverable
+			// TODO: PHP, USER: make a password reset page
+			// TODO: PHP, USER: don't show the register page for logged in users to prevent problems
+			// TODO: PHP, USER: create user page
+			// TODO: PHP, ADMIN: set up email verification as an admin option
+			// TODO: PHP, MISC: Auto login on registration
+
+			$pass = md5($_POST['upss']);
+
+			// Try and insert the new user
+			if($SQL->query('INSERT INTO `usr_accounts` VALUES (NULL,\''.$_POST['unme'].'\',2,\''.$_POST['mail'].'\',\''.$pass.'\',0,\'anon.png\')')) {
+
+				// If it went without any problems, say so and end.
+				$body .= '<p class="green">Registration Complete, you may now log in.</p>';
+				break;
+
+			}
+			// If we hit this, there was an sql problem
+			ERROR::report('SQL Error on user registration, database said: '.$SQL->error);
+			$e += 8;
+		}
+		$errors = array();
+
+		// The error code array
+		$ErrorCodes = array(
+		1 => 'Invalid Username',
+		2 => 'Invalid Email Address',
+		4 => 'Passwords do not match',
+		8 => 'SQL Error, reported to admins'
+		);
+
+		krsort($ErrorCodes,SORT_NUMERIC);
+
+		foreach($ErrorCodes as $k => $v) {
+			if($e >= $k) {
+				$e -= $k;
+				$errors[] = $v;
+			}
+		}
+
+		// Let the user know what went wrong
+		$body .= '<p class="error">';
+		$body .= implode("<br />\n",$errors);
+		$body .= '</p>';
+
+
+		// Set some vars before returning the form
+		$formVars['unme'] = $FORM->scrub_text($_POST['unme']);
+		$formVars['mail'] = $FORM->scrub_text($_POST['mail']);
+
+	}
+
+	// If we have reached this then just show the form
+	$body .= $regForm->returnForm();
+	break;
+
+case 'uac':
+	$form = new newForm();
+	$form->fieldStart('Avatar Settings');
+	$form->inputCheckbox('grav',1,'Use Gravatar?',FALSE,FALSE,TRUE);
+	$form->inputSubmit();
+	$body .= $form->returnForm();
+	break;
+
+default:
+	$body .= 'Why are you here again?';
+	break;
 }
 $body .= "\n".'</div>';
 $navbar = navbuild($SQL);
